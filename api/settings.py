b6ac8c52ae6907b4ed6bcf23e05c4aabe3799fe8
okputu julius <juliusstan10@gmail.com>
2022-10-28 17:59:03 +0100

    fix

--- api/settings.py
+++ api/settings.py
@@ -135,3 +135,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+from corsheaders.defaults import default_headers
+
+CORS_ALLOW_HEADERS = default_headers + (
+    'Access-Control-Allow-Origin',
+)
